ae98ecdee76a7197cf3b763f0d19076d809a523f
ian kopke <iankopke2005@Gmail.com>
2025-03-21 23:05:51 -0400

    Collection should be working

--- commands/collections.py
+++ commands/collections.py
@@ -18,6 +18,12 @@ def collection(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
         add_to_collection(conn, args[1::], ctx)
     elif args[0] == "remove":
         remove_from_collection(conn, args[1::], ctx)
+    elif args[0] == "show":
+        show_collections(conn, args[1::], ctx)
+    elif args[0] == "count":
+        count_collections(conn, args[1::], ctx)
+    elif args[0] == "rename":
+        rename_collection(conn, args[1::], ctx)    
     else: 
         return None
     
@@ -95,7 +101,11 @@ WHERE up.uid = %s -- Replace user_uid with the actual user's UID
     FROM collection
     WHERE name = %s;
 ''', (col_name,))
-        col_id = cur.fetchone()[0]
+        col_id = cur.fetchone()
+        if not col_id:
+            print("Collection does not exist")
+            return
+        col_id = col_id[0]
         print("col_id = ", col_id)
         cur.execute('''
 INSERT INTO collection_has_video_game (cid, vid)
@@ -118,21 +128,24 @@ def remove_from_collection(conn: psycopg.Connection, args: list[str], ctx: dict[
     FROM video_games
     WHERE title = %s;
     ''', (vg_name,))
-        vg_id = cur.fetchone()[0]
+        vg_id = cur.fetchone()
         print("vg_id = ", vg_id)
         if not vg_id:
             print("Video Game does not exist")
             return
+        vg_id = vg_id[0]
+
         cur.execute('''
     SELECT cid
     FROM collection
     WHERE name = %s;
 ''', (col_name,))
-        col_id = cur.fetchone()[0]    
+        col_id = cur.fetchone()  
         print("col_name = ", col_id)
         if not col_id:
             print("Collection does not exist")
             return
+        col_id = col_id[0]
         cur.execute('''
     DELETE FROM collection_has_video_game WHERE cid = %s AND vid = %s
             ''', (col_id, vg_id))
@@ -141,30 +154,54 @@ def remove_from_collection(conn: psycopg.Connection, args: list[str], ctx: dict[
 
 
 def delete_collection(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
-    cid = args[0]
-    with conn.cursor() as curr:
-        curr.execute(
+    col_name = input("Collection Name: ")
+    with conn.cursor() as cur:
+        cur.execute('''
+    SELECT cid
+    FROM collection
+    WHERE name = %s;
+''', (col_name,))
+        col_id = cur.fetchone() 
+        print("col_name = ", col_id)
+        if not col_id:
+            print("Collection does not exist")
+            return
+        col_id = col_id[0]
+    
+        cur.execute(
         '''
         DELETE 
         FROM collection c
         WHERE cid = %s;
         ''',
-        (cid))
+        (col_id))
     print(f'Deleted collection successfully')
     conn.commit()
 
-def rename_collection(conn: psycopg.Connection, args: list[str]):
-    name = args[0]
-    cid = args[1]
-    with conn.cursor() as curr:
-        curr.execute(
+def rename_collection(conn: psycopg.Connection, args: list[str],  ctx: dict[str, Any]):
+    old_name = input("Old collection Name: ")
+    new_name = input("New COllection Name:")
+    with conn.cursor() as cur:
+        cur.execute('''
+    SELECT cid
+    FROM collection
+    WHERE name = %s;
+''', (old_name,))
+        col_id = cur.fetchone()
+        # print("col_name = ", col_id)
+        if not col_id:
+            print("Collection does not exist")
+            return
+        col_id = col_id[0]
+
+        cur.execute(
         '''
         UPDATE collection
         SET name = %s
         WHERE cid = %s;
         ''',
-        (name, cid))
-    print(f'Renamed collection to {name} successfully')
+        (new_name, col_id))
+    print(f'Renamed collection to {new_name} successfully')
     conn.commit() 
 
 def count_collections(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
@@ -191,14 +228,14 @@ def count_collections(conn: psycopg.Connection, args: list[str], ctx: dict[str, 
         print(f'Rows:{version}')
 
 def show_collections(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
-    user_id = ctx['user_id']  # Assuming the user ID is passed as the first argument
+    user_id = ctx['uid']  # Assuming the user ID is passed as the first argument
     with conn.cursor() as curr:
         curr.execute(
         '''
         SELECT 
             c.name AS collection_name,
             COUNT(chvg.vid) AS number_of_video_games,
-            COALESCE(SUM(EXTRACT(EPOCH FROM (up.end_time - up.start)) / 3600), 0) AS total_playtime_hours
+            COALESCE(SUM(EXTRACT(EPOCH FROM (up.end_time - up.start_time)) / 3600), 0) AS total_playtime_hours
         FROM collection c
         LEFT JOIN user_has_collection uhc ON c.cid = uhc.cid
         LEFT JOIN collection_has_video_game chvg ON c.cid = chvg.cid
@@ -207,7 +244,7 @@ def show_collections(conn: psycopg.Connection, args: list[str], ctx: dict[str, A
         GROUP BY c.name
         ORDER BY c.name ASC;
         ''', 
-        (user_id))
+        (user_id,))
         results = curr.fetchall()
     conn.commit()
     return results
