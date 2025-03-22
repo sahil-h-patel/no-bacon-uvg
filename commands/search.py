d854d91fae6155857c7bdf8fdc6bf48e6cbdd1db
mscheper <gmaxscheper@gmail.com>
2025-03-21 23:12:09 -0400

    Technically works

--- commands/search.py
+++ commands/search.py
@@ -1,29 +1,7 @@
 import psycopg
 from typing import Any
 from datetime import datetime
-'''
-Users will be able to search for video games by 
-    name
-    platform
-    release date
-    developers
-    price
-    genre
-The resulting list of video games must show 
-    the video game’s name
-    platforms
-    the developers
-    the publisher
-    the playtime
-    the ratings (age and user)
-The list must be sorted alphabetically (ascending) by video game’s name and release date. 
-Users can sort the resulting list by: 
-    video game name
-    price
-    genre
-    released year (ascending and descending)
-'''
-
+import sys
 
 def search(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
     title = input("Title: ")
@@ -38,42 +16,41 @@ def search(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
 
     # Type checking
     if price == "":
-        price = "0"
+        price = sys.float_info.max
+    if releaseDateEnd == "":
+        releaseDateEnd = datetime.max
+    if releaseDateStart == "":
+        releaseDateStart = datetime.min
     
-    '''
+    # Get order style
+    orderby = ""
     print("1: by price\n2: by genre\n3: by release year")
     choice = input("Change order: ")
     match choice:
-        case "":
-            order = "ORDER BY title"
         case "1":
-            order = "ORDER BY price"
-        case "2":
-            order = "ORDER BY genre"
+            orderby = "ORDER BY\nvg_platform.price"
+        #case "2":
+        #    order = "ORDER BY genre ASC"
         case "3":
-            order = "ORDER BY released year"
+            orderby = "ORDER BY\nvg_platform.release_date"
+        case _:
+            orderby = "ORDER BY title ASC, vg_platform.release_date ASC"
+    
+    # Get ASC Vs. DESC
     print("1: ASC\n2: DESC")
     choice = input("ASC or DES: ")
     match choice:
         case "1":
-            order = order + "ASC"
+            orderby = orderby + " ASC"
         case "2":
-            order = order + "DESC"
-    '''
-    
-    order = """ORDER BY title ASC"""
+            orderby = orderby + "  DESC"
+
     with conn.cursor() as cur:
         cur.execute("""
-            SELECT
-                vg.Title AS video_game_name,
-                p.name AS platform_name,
-                d.name AS developer_name,
-                pub.name AS publisher_name,
-                vg_platform.price AS Price,
-                vg_platform.release_date AS release_date,
-                vg.ESRB AS age_rating,
-                ur.rating AS user_rating,
-                AVG(EXTRACT(EPOCH FROM (up.end_time - up.start)) / 3600) AS average_playtime_hours
+            SELECT DISTINCT
+                SUM(up.end_time - up.start_time) AS total_time,
+                vg.vid AS vgid,
+                vg.Title AS video_game_name
             FROM
                 video_games vg
             JOIN
@@ -83,7 +60,7 @@ def search(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
             LEFT JOIN
                 video_game_developer vg_dev ON vg.vid = vg_dev.vid
             LEFT JOIN
-                contributor d ON vg_dev.dpid = d.dpid
+                contributor dev ON vg_dev.dpid = dev.dpid
             LEFT JOIN
                 video_game_publisher vg_pub ON vg.vid = vg_pub.vid
             LEFT JOIN
@@ -99,16 +76,29 @@ def search(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
             WHERE
                 vg.Title ILIKE %s ESCAPE ''
                 AND p.name ILIKE %s ESCAPE ''
-                AND d.name ILIKE %s ESCAPE ''
+                AND dev.name ILIKE %s ESCAPE ''
                 AND pub.name ILIKE %s ESCAPE ''
                 AND g.genre ILIKE %s ESCAPE ''
                 AND vg_platform.price <= %s
                 AND vg_platform.release_date >= %t
                 AND vg_platform.release_date <= %t
             GROUP BY
-                vg.Title, p.name, d.name, pub.name, vg_platform.price, vg_platform.release_date, vg.ESRB, ur.rating;
+                vgid, video_game_name
             """,("%"+title+"%","%"+platform+"%","%"+developer+"%","%"+publisher+"%","%"+genre+"%",int(price),releaseDateStart,releaseDateEnd))
         
         work = cur.fetchall()
         for i in work:
-            print(i)
+            print("Name: " + str(i[2]))
+            print("Time Played: " + str(i[0]))
+            cur.execute("SELECT * FROM platform WHERE pid IN (SELECT pid FROM video_game_platforms WHERE vid=%s)",(i[1],))
+            print("Platforms: " + str(cur.fetchall()[0]))
+            cur.execute("SELECT * FROM contributor WHERE dpid IN (SELECT dpid FROM video_game_developer WHERE vid=%s)",(i[1],))
+            print("Developers: " + str(cur.fetchall()[0]))
+            cur.execute("SELECT * FROM contributor WHERE dpid IN (SELECT dpid FROM video_game_publisher WHERE vid=%s)",(i[1],))
+            print("Producers: " + str(cur.fetchall()[0]))
+            cur.execute("SELECT u.username, ur.rating FROM user_rating AS ur, users AS u WHERE ur.vid=%s AND u.uid=ur.uid",(i[1],))
+            print("Ratings: " + str(cur.fetchall()[0]))
+            cur.execute("SELECT * FROM genre AS g WHERE g.gid IN (SELECT gid FROM video_game_genre WHERE vid=%s)",(i[1],))
+            print("Genres: " + str(cur.fetchall()[0]))
+            print()
+        
