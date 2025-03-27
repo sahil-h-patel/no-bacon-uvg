3c8356899a2c45c2a38b25cd721f751795db281b
sahil <shp2382@g.rit.edu>
2025-03-27 11:43:55 -0400

    init recommend branch

---
+++ commands/recommend.py
@@ -0,0 +1,14 @@
+import psycopg
+from typing import Any
+
+def top_20_rolling(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
+    pass
+
+def top_20_followers(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
+    pass
+
+def top_5_releases_month(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
+    pass
+
+def for_you(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
+    pass
