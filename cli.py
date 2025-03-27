7bf263a65afc5569096624da1847a3cd98725105
mscheper <gmaxscheper@gmail.com>
2025-03-27 17:50:54 -0400

    Top 20 rolling works

--- cli.py
+++ cli.py
@@ -19,7 +19,8 @@ from commands import (
     play_random,
     rate,
     search,
-    top_5_releases_month
+    top_5_releases_month,
+    top_20_rolling
 )
 
 PROMPT = "nbuvg> "
@@ -37,6 +38,7 @@ CMDS: dict[str, Callable[[psycopg.Connection, list[str], dict[str, Any]], None]]
     "unfollow": unfollow,
     "create_account": create_account,
     "top_5_monthly" : top_5_releases_month,
+    "top_20_rolling" : top_20_rolling
 }
 
 CONTEXT: dict[str, Any] = {}
