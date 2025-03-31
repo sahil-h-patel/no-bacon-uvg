7b6bf8bcb3e4f06f0aa930c53c720c854d1c45d1
ian kopke <iankopke2005@Gmail.com>
2025-03-31 19:59:33 -0400

    Addressed requested changes

--- commands/profile.py
+++ commands/profile.py
@@ -5,7 +5,7 @@ from commands.collections import count_collections
 from commands.follow import count_followers, count_users_you_follow
 
 def profile(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
-    if not ctx["uid"]:
+    if "uid" not in ctx:
         print("You're not logged in")
         return
     with conn.cursor() as cur:
@@ -16,7 +16,7 @@ def profile(conn: psycopg.Connection, args: list[str], ctx: dict[str, Any]):
         # Top 10 Video Games
         followers = count_followers(conn, args, ctx)
         you_follow = count_users_you_follow(conn, args, ctx)
-        print(f"You Follow {you_follow} many Users, and {followers} follow you\n")
+        print(f"You Follow {you_follow} Users, and {followers} follow you\n")
         print("Your top 10 Video Games by Playtime")
 
         cur.execute('''
@@ -40,7 +40,7 @@ LIMIT 10;
         result = cur.fetchall()
         # print(result)
         for res in result:
-            print(f"\tVideo Game: {res[0]}; Playtime: {res[1]}")
+            print(f"\tVideo Game: {res[0]}; Playtime: {res[1]:2f}")
         if not result:
             print("Such empty, go play games")
         print("\n")
